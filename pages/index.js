726fdf347bd472a0fa14530e50daea53fec74f00
Noor Fadhila Kamal <noor.fadhila05@gmail.com>
2022-02-22 12:40:20 +0700

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import React from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+
+var opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/jpeg',
+  quality: 0.3,
+  margin: 1,
+  color: {
+    dark:"#010599FF",
+    light:"#FFBF60FF"
+  }
+}
 
 export default function Home() {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +31,15 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button type="button" onClick={() => router.push('/generate')}>
+          GENERATE QR CODE
+        </button>
+        <button type="button" onClick={() => router.push('/capture')}>
+          CAPTURE
+        </button>
+        
       </main>
 
       <footer className={styles.footer}>
